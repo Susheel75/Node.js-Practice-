66f3ae62ec153b8c674dec09cbfbbf8e85ae504a
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2025-02-23 21:57:36 +0530

    Add files via upload

---
+++ Creating Routes Using Express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello World, This is our home page')
+
+});
+
+app.get('/about',(req,res)=>{
+    res.send('<h1>Hello World, This is our about page</h1>')
+
+});
+
+app.get('/contact',(req,res)=>{
+    res.send('<h1>Hello World, This is our contact page</h1>')
+
+});
+
+app.get('/services',(req,res)=>{
+    res.send('<ul><li>Web Design</li><li>Logo Design</li><li>Video Creation</li></ul>')
+
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
